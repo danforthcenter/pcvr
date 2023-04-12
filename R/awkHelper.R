7d0c88f42b274bb8370d2beb1a5a33e1eecab583
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-04-12 08:38:39 -0500

    added regex filtering to possible awkHelper filtering words with "contains"

--- R/awkHelper.R
+++ R/awkHelper.R
@@ -1,18 +1,18 @@
 #' subset helper function for use reading in large data, called in pcv.sub.read
 #' 
-#' Idea with awkHelperRegex branch is to add a new option for the in/is/= syntax, probably "contains"
-#' which would be used like "trait contains frequency" ~ grepl(".*?frequency.*?")
-#' Since a part of awkHelper only involves reading in the first line of the file it would
-#' not work to say "find all the things with this string", then parse them into || statements for awk.
-#' Instead I should use the regex options in awk through `\string\`
-#' 
 #' 
 #' 
 #' @param inputFile Path to csv file of plantCV output, should be provided internally in read.pcv
-#' @param filters filtering conditions, see read.pcv for details. Format as list("trait in area, perimeter", "other in value")
+#' @param filters filtering conditions, see read.pcv for details. Format as list("trait in area, perimeter", "other contains stringToMatch")
 #' @param awk Optional awk command to use instead.
 #' @keywords read.csv, pcv, wide, long
 #' @details awkHelper attempts to make awk commands from human readable input. Currently when filters are supplied the input file has quotes removed by `sed` then is piped into awk, so an equivalent command line statement may be: sed 's/\"//g' pcvrTest2.csv | awk -F ','  '{ if (NR==1 || $18=="area") { print } }'
+#' @examples 
+#' inputFile = "/home/jsumner/Desktop/stargate/fahlgren_lab/pcvrTestData/pcvrTest1.csv"
+#' filters = "trait contains area, perimeter"
+#' cat(awkHelper(inputFile, filters)) # sed 's/"//g' /home/jsumner/Desktop/stargate/fahlgren_lab/pcvrTestData/pcvrTest1.csv |  awk -F  ','  '{ if ( ( ($18 ~ /area|perimeter/) ) ) { print } }'
+#' filters = list("trait in area, width, height")
+#' cat(awkHelper(inputFile, filters)) # sed 's/"//g' /home/jsumner/Desktop/stargate/fahlgren_lab/pcvrTestData/pcvrTest1.csv |  awk -F  ','  '{ if ( ( ($18=="area") || ($18=="width") || ($18=="height") ) ) { print } }'
 #' @export
 awkHelper<-function(inputFile, filters, awk=NULL){
   if(is.null(awk)){
@@ -25,14 +25,17 @@ awkHelper<-function(inputFile, filters, awk=NULL){
     COLS = colnames(read.csv(inputFile, nrows=1))
     awkFilts<-lapply(filters, function(filt){
       filtCol = strsplit(filt," ")[[1]][1]
-      filt<-gsub("( = )|( is )", " in ", filt)
-      values = trimws(strsplit(trimws(strsplit(filt," in ")[[1]][-1]),",")[[1]])
-      paste(paste0("($", which(COLS == filtCol), '=="', values,'")' ), collapse=" || ")
+      affector<-strsplit(filt," ")[[1]][2]
+      values<-trimws(gsub(",", "", strsplit(filt," ")[[1]][-c(1:2)]))
+      if(affector %in% c("in", "is", "=")){
+        paste(paste0("($", which(COLS == filtCol), '=="', values,'")' ), collapse=" || ")
+      } else if(affector=="contains"){
+        valReg<-paste0(values, collapse="|")
+        paste0("($", which(COLS==filtCol), " ~ /", valReg, "/)")
+      }
     })
     awkFilt = paste(paste("(",awkFilts,")"), collapse = " && ")
     awkCommand<-capture.output(cat(sed, awkStart, awkDelim, awkFiltStart, awkFilt, awkFiltEnd))
   } else {awkCommand = awk}
   return(awkCommand)
 }
-# sed 's/"//g' /home/jsumner/Desktop/stargate/fahlgren_lab/pcvrTestData/pcvrTest1.csv |  awk -F  ','  '{ if ($18 ~ /area|perimeter/)  { print } }'
-# sed 's/"//g' /home/jsumner/Desktop/stargate/fahlgren_lab/pcvrTestData/pcvrTest1.csv |  awk -F  ','  '{ if ($18 ~ /^area$|^perimeter$/)  { print } }'|head
