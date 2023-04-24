7a9ee1082efc6cdd7d7798ae8e5be6e727aad680
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-04-24 10:55:36 -0500

    Update emd.R
    Parallel option for returning matrix

--- R/emd.R
+++ R/emd.R
@@ -5,7 +5,7 @@
 #' @param df Data frame to use with multi value traits in wide format
 #' @param cols Columns to use. Defaults to NULL in which case all columns are used. Single strings will be used to regex a pattern in column names (see examples). A vector of names, positions, or booleans will also work.
 #' @param reorder Should data be reordered to put similar rows together in the resulting plot? This takes a vector of column names of length 1 or more (see examples).
-#' @param include if a long dataframe is returned then these columns will be added to the dataframe, labelled for i and j (the row positions for compared histograms).
+#' @param include if a long dataframe is returned then these columns will be added to the dataframe, labelled for i and j (the row positions for compared histograms). If a matrix is returned then this information is stored in the row names.
 #' @param mat Logical, should data be returned as an nrow x nrow matrix or as a long dataframe? By Default this is FALSE and a long dataframe is returned. Both options are comparable in terms of speed, although for large datasets the matrix version may be slightly faster.
 #' @param plot Logical, should a plot be returned? For a matrix this is made with image(), for a dataframe this uses ggplot.
 #' @param parallel Number of cores to use. If this is above 1 then \code{parallel::mclapply} is used with this number of cores.
@@ -38,13 +38,10 @@ pcv.emd<-function(df, cols=NULL, reorder=NULL, include=NULL, mat=F, plot = T, pa
     }else if(is.character(cols) && length(cols)==1){cols<-grepl(cols, colnames(df))}
   if(parallel > 1){innerLapply <- function(...){parallel::mclapply(..., mc.cores=parallel)}}else{innerLapply<-lapply}
   if(mat){
-    out_data<-matrix(0, nrow=nrow(df), ncol = nrow(df))
-    for(i in 1:nrow(df)){
-      for(j in 1:nrow(df)){
-        if(i==j){emd<-0}else{emd<-emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
-        out_data[i,j]<-emd
-      }
-    }
+    out_data<-matrix(
+      unlist(lapply(1:nrow(df), function(i){innerLapply(1:nrow(df), function(j){
+        if(i==j){0}else{emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
+      })})), nrow=nrow(df), ncol = nrow(df) )
     if(plot){
       p<-image(out_data)
     }
