15c067241b23361637f74ab7d490a3088a60a7a9
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-05-01 13:27:45 -0500

    changing pcv.emd to work with long or wide data

--- R/emd.R
+++ R/emd.R
@@ -3,12 +3,15 @@
 #' @description pcv.emd can be used to calculate Earth Mover's Distance between pairwise histograms in a wide dataframe of multi value traits.
 #' 
 #' @param df Data frame to use with multi value traits in wide format
-#' @param cols Columns to use. Defaults to NULL in which case all columns are used. Single strings will be used to regex a pattern in column names (see examples). A vector of names, positions, or booleans will also work.
+#' @param cols Columns to use. Defaults to NULL in which case all columns are used. Single strings will be used to regex a pattern in column names (see examples). A vector of names, positions, or booleans will also work. For long data this is taken as a regex pattern (or full name) to use in filtering the longTrait column.
 #' @param reorder Should data be reordered to put similar rows together in the resulting plot? This takes a vector of column names of length 1 or more (see examples).
 #' @param include if a long dataframe is returned then these columns will be added to the dataframe, labelled for i and j (the row positions for compared histograms). If a matrix is returned then this information is stored in the row names. This defaults to \link{rescale}.
 #' @param mat Logical, should data be returned as an nrow x nrow matrix or as a long dataframe? By Default this is FALSE and a long dataframe is returned. Both options are comparable in terms of speed, although for large datasets the matrix version may be slightly faster.
 #' @param plot Logical, should a plot be returned? For a matrix this is made with image(), for a dataframe this uses ggplot.
 #' @param parallel Number of cores to use. If this is above 1 then \code{parallel::mclapply} is used with this number of cores.
+#' @param longTrait Defaults to NULL, in which case the data is assumed to be in long format. If this is a character string then it is taken as a column name of long data and the other arguments will assume data is long.
+#' @param id A vector of column names that uniquely identifies observations if the data is in long format. Defaults to "image".
+#' @param value A column name for the values to be drawn from in long data. Defaults to "value".
 #' @import ggplot2
 #' 
 #' @keywords emd, earth mover's distance, multi-value trait, histogram
@@ -28,37 +31,66 @@
 #' file = "https://raw.githubusercontent.com/joshqsumner/pcvrTestData/main/pcvrTest1.csv"
 #' df1<-read.pcv(file, "wide", T, multiValPattern = c("index_frequencies_index_ari", "index_frequencies_index_ci_rededge", "npq_hist_NPQ", "yii_hist_Fq'/Fm'", "yii_hist_Fv/Fm"))
 #' colnames(df1)<-sub("index_frequencies_index_ndvi.", "ndvi_", colnames(df1))
-#' pcv.emd(df1, cols="ndvi_", reorder=c("treatment", "genotype"), mat =F, plot=T, parallel = 1)
-#' 
+#' w<-pcv.emd(df1, cols="ndvi_", reorder=c("treatment", "genotype"), mat =F, plot=T, parallel = 1)
+#' df_long<-read.pcv(file, "long", F)
+#' l<-pcv.emd(df = df_long, cols="index_frequencies_index_ndvi", reorder=c("treatment", "genotype"), mat =F, plot=T, longTrait="trait", id="image", value="value")
+#' l$plot + theme(axis.text = element_blank())
 #' @export
 #' 
-pcv.emd<-function(df, cols=NULL, reorder=NULL, include=reorder, mat=F, plot = T, parallel = 1){
+pcv.emd<-function(df, cols=NULL, reorder=NULL, include=reorder, mat=F, plot = T, parallel = 1, longTrait=NULL, id="image", value="value"){
+  # df = df1; cols="ndvi_"; reorder=c("treatment", "genotype"); mat =F; plot=T; parallel = 1; include=reorder; longTrait=F; id="image";value="value"
+  # df_long<-read.pcv(file, "long", F)
+  # df = df_long; cols="index_frequencies_index_ndvi"; reorder=c("treatment", "genotype"); mat =F; plot=T; parallel = 1; include=reorder;
+  # longTrait="trait"; id="image"; value="value"
+  if(!is.null(longTrait)){traitCol = longTrait ; long=T}else{long=F}
   if(!is.null(reorder)){ df<-df[order(interaction(df[,reorder])),] }
-  if(is.null(cols)){cols<-colnames(df)
-    }else if(is.character(cols) && length(cols)==1){cols<-grepl(cols, colnames(df))}
   if(parallel > 1){innerLapply <- function(...){parallel::mclapply(..., mc.cores=parallel)}}else{innerLapply<-lapply}
-  if(mat){
-    out_data<-matrix(
-      unlist(lapply(1:nrow(df), function(i){innerLapply(1:nrow(df), function(j){
-        if(i==j){0}else{emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
-      })})), nrow=nrow(df), ncol = nrow(df) )
-    if(plot){
-      p<-image(out_data)
-    }
-    if(!is.null(include)){
-      rownames(out_data)<-interaction(df[,include])
-    }
-  }else{
-    out_data<-do.call(rbind, lapply(1:nrow(df), function(i){
-      do.call(rbind, innerLapply(1:nrow(df), function(j){
-        if(i==j){emdOut=0}else{emdOut=emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
-        if(!is.null(include)){x<-data.frame(i=i, j=j, emd = emdOut, df[i, include], df[j,include])
+  if(long){
+    df<-df[grepl(cols, df[[traitCol]]), ]
+    df$INNER_ID_EMD<-interaction(df[,id], drop=T)
+    if(mat){ # make dist matrix
+      out_data<-matrix(
+        unlist(lapply(unique(df$INNER_ID_EMD), function(i){innerLapply(unique(df$INNER_ID_EMD), function(j){
+          if(i==j){0}else{emd1d(as.numeric(df[df$INNER_ID_EMD==as.character(i), value]), as.numeric(df[df$INNER_ID_EMD==as.character(j), value]))}
+        })})), nrow=length(unique(df$INNER_ID_EMD)), ncol = length(unique(df$INNER_ID_EMD)) )
+    }else{ # make long data
+      out_data<-do.call(rbind, lapply(unique(df$INNER_ID_EMD), function(i){
+        do.call(rbind, innerLapply(unique(df$INNER_ID_EMD), function(j){
+          if(i==j){emdOut=0}else{emdOut=emd1d(as.numeric(df[df$INNER_ID_EMD==as.character(i), value]), as.numeric(df[df$INNER_ID_EMD==as.character(j), value]) )}
+          if(!is.null(include)){x<-data.frame(i=i, j=j, emd = emdOut, df[df$INNER_ID_EMD==as.character(i), include], df[df$INNER_ID_EMD==as.character(j), include])
           colnames(x)<-c("i", "j", "emd", paste0(include,"_i"), paste0(include, "_j"))
           } else {x<-data.frame(i=i, j=j, emd = emdOut)}
-        x
+          x
+        }))
       }))
-    }))
-    if(plot){
+    }
+  } else{
+      if(is.null(cols)){cols<-colnames(df)
+        }else if(is.character(cols) && length(cols)==1){cols<-grepl(cols, colnames(df))}
+      if(mat){# make dist matrix
+        out_data<-matrix(
+          unlist(lapply(1:nrow(df), function(i){innerLapply(1:nrow(df), function(j){
+            if(i==j){0}else{emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
+          })})), nrow=nrow(df), ncol = nrow(df) )
+        if(!is.null(include)){
+          rownames(out_data)<-interaction(df[,include])
+        }
+      }else{# make long dataframe
+        out_data<-do.call(rbind, lapply(1:nrow(df), function(i){
+          do.call(rbind, innerLapply(1:nrow(df), function(j){
+            if(i==j){emdOut=0}else{emdOut=emd1d(as.numeric(df[i, cols]), as.numeric(df[j, cols]))}
+            if(!is.null(include)){x<-data.frame(i=i, j=j, emd = emdOut, df[i, include], df[j,include])
+              colnames(x)<-c("i", "j", "emd", paste0(include,"_i"), paste0(include, "_j"))
+              } else {x<-data.frame(i=i, j=j, emd = emdOut)}
+            x
+          }))
+        }))
+      }
+  }
+  if(plot){
+    if(mat){
+      p<-image(out_data)
+    }else{
       p<-ggplot2::ggplot(out_data, ggplot2::aes(x=i, y=j, fill=emd))+
         ggplot2::geom_tile(color=NA)+
         ggplot2::labs(fill = "Earth Mover's Distance")+
