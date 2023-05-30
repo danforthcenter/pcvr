cca9b1b0218ba9d5572581bf5797e49ac77f5b52
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-05-30 08:20:52 -0500

    adding options for interaction variables in netplot

--- R/netplot.R
+++ R/netplot.R
@@ -4,8 +4,8 @@
 #' 
 #' 
 #' @param net Network object similar to that returned from pcv.net, having dataframes named "edges" and "nodes" 
-#' @param fill A variable name from the nodes data to be used to color points. By default "strength" is used.
-#' @param shape An optional discrete variable name from the nodes data to be used to change the shape of points.
+#' @param fill Variable name(s) from the nodes data to be used to color points. By default "strength" is used.
+#' @param shape Optional discrete variable name(s) from the nodes data to be used to change the shape of points.
 #' @param size Size of points, defaults to 3.
 #' @param edgeWeight Edge dataframe column to weight connections between nodes. Defaults to "emd" for compatability with \code{pcv.emd}.
 #' @param edgeFilter How should edges be filtered? This can be either a numeric (0.5) in which case it is taken as a filter where only edges with values greater than or equal to that number are kept or a character string ("0.5") in which case the strongest X percentage of edges are kept. This defaults to NULL which does no filtering, although that should not be considered the best standard behaviour. See details.
@@ -32,7 +32,15 @@ net.plot<-function(net, fill="strength", shape=NULL, size = 3, edgeWeight="emd",
   nodes<-net[["nodes"]]
   edges<-net[["edges"]]
   if(is.null(fill)){fill="NOFILL"; edges$NOFILL="a"}
+  if(length(fill)>1){
+    edges$FILL = interaction(edges[,fill])
+    fill="FILL"
+  }
   if(is.null(shape)){shape = "NOSHAPE"; nodes$NOSHAPE="a"}
+  if(length(shape)>1){
+    nodes$SHAPE = interaction(nodes[,shape])
+    fill="SHAPE"
+  }
   if(!is.null(edgeFilter)){
     if(is.character(edgeFilter)){
       cutoff<-quantile(edges[[edgeWeight]], probs = as.numeric(edgeFilter))
