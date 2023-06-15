06c5be0de74eab78a4eebb10d4d4fed11b93435f
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-06-15 10:22:12 -0500

    small change to example

--- R/net.R
+++ R/net.R
@@ -19,7 +19,7 @@
 #' file = "https://raw.githubusercontent.com/joshqsumner/pcvrTestData/main/pcvrTest1.csv"
 #' df1<-read.pcv(file, "wide", T, multiValPattern = c("index_frequencies_index_ari", "index_frequencies_index_ci_rededge", "npq_hist_NPQ", "yii_hist_Fq'/Fm'", "yii_hist_Fv/Fm"))
 #' colnames(df1)<-sub("index_frequencies_index_ndvi.", "ndvi_", colnames(df1))
-#' emd_df<-pcv.emd(df1, cols="ndvi_", reorder=c("treatment", "genotype", "X"), mat =F, plot=F, parallel = 1)
+#' emd_df<-pcv.emd(df1, cols="ndvi_", reorder=c("treatment", "genotype"), mat =F, plot=F, parallel = 1)
 #' net<-pcv.net(emd_df, meta = c("treatment", "genotype"))
 #' 
 #' @return Returns a list containing three elements:
@@ -31,7 +31,7 @@
 #' 
 
 pcv.net<-function(emd = NULL, meta = NULL, dissim=T, distCol="emd", filter = NULL, direction="greater"){
-  #* emd = emd_df; meta = c("treatment", "genotype"); dissim=T; distCol="emd"
+  #* emd = emd_df; meta = c("genotype", "treatment"); dissim=T; distCol="emd"; direction="greater";filter=NULL
   #* format distCol into a similarity col
   if(is.data.frame(emd)){
     #* convert to dissimilarity if metric is similarity
