549cea0f16f9fd5916118b08345cac08abed4b20
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-07-07 14:36:57 -0500

    changing plotPrior to be optionally show simulated data using the prior distribution and a growth model

--- R/plotPrior.R
+++ R/plotPrior.R
@@ -1,26 +1,182 @@
 #' Check priors used in ease of use brms functions
 #' 
-#' @param priors A named list of means for prior distributions. This does not support vectors for each element of the list or brmsprior objects, but otherwise should take the same input as \code{\link{growthSS}}.
-#' @keywords Bayesian, brms
-#' @return A named list of plots showing prior distributions that \code{growthSS} would make with these inputs.
+#' @param priors A named list of means for prior distributions. This takes the same input as the prior argument of \code{\link{growthSS}}.
+#' @param type Either "density", the default, or a model name from \code{growthSS} model options ("logistic", "gompertz", "monomolecular", "exponential", "linear", "power law").
+#' If this is a model type then n draws from the prior will be simulated as growth trendlines and densities will be plotted on margins.
+#' @param n Numeric, if type is a model then how many draws from the prior should be simulated?
+#' @param t Numeric, time passed to growthSim. Defaults to 25 (the growthSim default).
+#' @keywords Bayesian, brms, prior
+#' @return A named list of plots showing prior distributions that \code{growthSS} would use, optionally with a plot of simulated growth curves using draws from those priors.
 #' @import ggplot2
+#' @import patchwork
 #' @examples 
 #' 
-#' priors = list("A" = 130, "B" = 10, "C" = 1.2)
-#' priorCheck(priors)
+#' ## Not run:
+#'
+#' set.seed(123)
+#' priors = list("A" = c(100, 130), "B" = c(10, 8), "C" = c(0.2, 0.1))
+#' plotPrior(priors)
 #' 
+#' plotPrior(priors, "gompertz")[[1]]
+#' 
+#' plotPrior(list("A" = c(100, 150), "B" = c(6, 12), "C" = c(3, 2.5)), "logistic")[[1]]
+#' 
+#' plotPrior(list("A" = c(120, 150), "B" = c(0.1, 0.08)), "monomolecular")[[1]]
+#'
+#' plotPrior(list("A" = c(9, 5), "B" = c(0.05, 0.07)), "exponential")[[1]]
+#'
+#' plotPrior(list("A" = c(1.25, 1)), "linear")[[1]]
+#'
+#' plotPrior(list("A" = c(20, 15), "B" = c(0.7, 0.7)), "power law")[[1]]
+#'
+#' ## End(Not run:)
+#'
 #' @export
 
-plotPrior<-function(priors){
-  plots<-lapply(1:length(priors), function(i){
+plotPrior<-function(priors, type = "density", n=1000, t=25){
+  
+  densPlots<-lapply(1:length(priors), function(i){
     pri=priors[[i]]
     nm=names(priors)[i]
-    max=round(max(rlnorm(1000, log(pri), 0.25))*1.1, 0)
-    ggplot2::ggplot(data = data.frame(x=seq(0,max,length.out=10000)), ggplot2::aes(x=x))+
-      ggplot2::stat_function(fun=dlnorm, geom="polygon", fill="gray40",color="black",
-                             args=list(meanlog = log(pri), sdlog=0.25, log=F))+
-      ggplot2::theme_minimal()+ggplot2::labs(y="Density", title=nm)
+    
+    pri_df <- do.call(rbind, lapply(1:length(pri), function(o){
+      prio <- pri[o]
+      max=ceiling(max(rlnorm(1000, log(max(pri)), 0.25))*1.1)
+      support = seq(0,max,length.out=10000)
+      dens = dlnorm(support, meanlog = log(prio), sdlog=0.25)
+      pdf = dens/sum(dens)
+      data.frame(support = support,
+                 dens = pdf,
+                 param=nm,
+                 item = as.character(o))
+    }))
+    
+    ggplot2::ggplot(pri_df, ggplot2::aes(x = support, y = dens, fill=item, group=item))+
+      ggplot2::geom_polygon(alpha = 0.5)+
+      ggplot2::theme_minimal()+ggplot2::labs(y="Density", title=nm, fill="Prior")
+    
   })
-  names(plots)<-names(priors)
-  return(plots)
+  names(densPlots)<-names(priors)
+
+  if(type=="density"){
+    out<-densPlots
+  } else{
+    
+    type = match.arg(type, c("logistic", "gompertz", "monomolecular", "exponential", "linear", "power law"))
+    
+    x_margin_plot = NULL
+    y_margin_plot = NULL
+    
+    if(type=="logistic"){
+      
+      simdf <- do.call(rbind, lapply(1:n,  function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "logistic", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } )) # simulate data using prior draw
+      x_margin_plot = densPlots[["B"]] +
+        ggplot2::labs(x="Inflection Point Prior")+
+        ggplot2::theme(plot.title = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(),
+                       axis.text.y = ggplot2::element_blank(),axis.title.y = ggplot2::element_blank(),
+                       legend.position = "none" )
+      y_margin_plot = densPlots[["A"]] + ggplot2::scale_y_reverse(position = "right")+ 
+        ggplot2::scale_x_continuous(position = "top")+
+        ggplot2::labs(x="Asymptote Prior")+
+        ggplot2::coord_flip() + 
+        ggplot2::theme(plot.title = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(),
+                       axis.text = ggplot2::element_blank(),axis.title.x = ggplot2::element_blank(),
+                       legend.position = "none" )
+      
+      } else if(type=="gompertz"){
+        
+      simdf <- do.call(rbind, lapply(1:n, function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "gompertz", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } ))
+      x_margin_plot = densPlots[["B"]] +
+        ggplot2::labs(x="Inflection Point Prior")+
+        ggplot2::theme(plot.title = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(),
+                       axis.text.y = ggplot2::element_blank(),axis.title.y = ggplot2::element_blank(),
+                       legend.position = "none" )
+      y_margin_plot = densPlots[["A"]] + ggplot2::scale_y_reverse(position = "right")+ 
+        ggplot2::scale_x_continuous(position = "top")+
+        ggplot2::labs(x="Asymptote Prior")+
+        ggplot2::coord_flip() + 
+        ggplot2::theme(plot.title = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(),
+                       axis.text = ggplot2::element_blank(),axis.title.x = ggplot2::element_blank(),
+                       legend.position = "none" )
+      
+      } else if(type=="monomolecular"){
+        
+      simdf <- do.call(rbind, lapply(1:n,  function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "monomolecular", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } ))
+      y_margin_plot = densPlots[["A"]] + ggplot2::scale_y_reverse(position = "right")+ 
+        ggplot2::scale_x_continuous(position = "top")+
+        ggplot2::labs(x="Asymptote Prior")+
+        ggplot2::coord_flip() + 
+        ggplot2::theme(plot.title = ggplot2::element_blank(), panel.grid = ggplot2::element_blank(),
+                       axis.text = ggplot2::element_blank(),axis.title.x = ggplot2::element_blank(),
+                       legend.position = "none" )
+      
+      } else if(type=="exponential"){
+      simdf <- do.call(rbind, lapply(1:n,  function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "exponential", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } ))
+
+      } else if(type=="linear"){
+      simdf <- do.call(rbind, lapply(1:n,  function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "linear", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } ))
+
+      } else if(type=="power law"){
+      simdf <- do.call(rbind, lapply(1:n,  function(i) {
+        iter_params <- .prior_sampler(priors)
+        x<-growthSim(model = "power law", n = 1, t = t, params = iter_params); x$id = paste0("id_",i);x } ))
+    
+      }
+    
+    model_plot<-ggplot2::ggplot(simdf,
+                    ggplot2::aes(x= time, y=y,group=interaction(id,group), color=group))+
+      ggplot2::geom_line(linewidth=0.1)+
+      ggplot2::theme_minimal()+
+      ggplot2::guides(color = ggplot2::guide_legend(override.aes = list(linewidth=5)))+
+      ggplot2::labs(y="Y", title=paste0(n," curves simulated from prior draws"),
+                    color="Prior")
+    
+    if(!is.null(x_margin_plot) & !is.null(y_margin_plot)){
+      
+      design = c(patchwork::area(1,1,6,6), # model plot
+                 patchwork::area(7,1,7,6), # x margin
+                 patchwork::area(1,7,6,7)) # y margin
+      model_plot <- model_plot + x_margin_plot + y_margin_plot + patchwork::plot_layout(design = design)
+      
+    } else if(!is.null(y_margin_plot) & is.null(x_margin_plot)){
+      
+      design = c(patchwork::area(1,1,6,6), # model plot
+                 patchwork::area(1,7,6,7)) # y margin
+      model_plot <- model_plot + y_margin_plot + patchwork::plot_layout(design = design)
+      
+    } else if(is.null(y_margin_plot) & !is.null(x_margin_plot)){
+      
+      design = c(patchwork::area(1,1,6,6), # model plot
+                 patchwork::area(7,1,7,6)) # x margin
+      model_plot <- model_plot + x_margin_plot + patchwork::plot_layout(design = design)
+      
+    }
+    out<-list("simulated"= model_plot, "distributions" = densPlots)
+  }
+
+  return(out)
+}
+
+#' @description
+#' Internal function for drawing from priors
+#' @param priors priors as a list
+#' @keywords internal
+#' @noRd
+
+.prior_sampler<-function(priors){
+  lapply(priors, function(pri){ # draw sample from prior
+    unlist(lapply(pri, function(mu){
+      rlnorm(1,log(mu), 0.25)
+    }))
+  })
 }
