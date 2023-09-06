f606924b5875816e6ea63dd9352ac932c95b9ec9
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2023-09-06 15:00:54 -0500

    adding better test titles and more conj options to test

--- tests/testthat/test-conjugate.R
+++ tests/testthat/test-conjugate.R
@@ -1,4 +1,4 @@
-test_that("conjugate single value T works", {
+test_that("conjugate single value T conjugate works", {
   s1 = c(43.8008289810423, 44.6084228775479, 68.9524219823026, 77.442231894233, 
          45.2302703709121, 53.8005757403944, 33.8292993277826, 59.7018653972819, 
          57.8433869136181, 52.8224097917938) # dput(rnorm(10, 50,10))
@@ -17,7 +17,7 @@ test_that("conjugate single value T works", {
   expect_equal(names(out), c("summary", "posterior") )
 })
 
-test_that("conjugate multi value T works", {
+test_that("conjugate multi value T conjugate works", {
   makeMvGauss<-function(bins=180,mu,sigma){
      setNames(data.frame(matrix(hist(rnorm(2000,mu, sigma),
       breaks=seq(1,bins,1), plot=FALSE)$counts, nrow=1)),
@@ -39,7 +39,7 @@ test_that("conjugate multi value T works", {
   expect_equal(names(out), c("summary", "posterior") )
 })
 
-test_that("conjugate single value gaussian works", {
+test_that("conjugate single value gaussian conjugate works", {
   s1 = c(43.8008289810423, 44.6084228775479, 68.9524219823026, 77.442231894233, 
          45.2302703709121, 53.8005757403944, 33.8292993277826, 59.7018653972819, 
          57.8433869136181, 52.8224097917938) # dput(rnorm(10, 50,10))
@@ -61,7 +61,7 @@ test_that("conjugate single value gaussian works", {
   expect_equal(names(out), c("summary", "posterior") )
 })
 
-test_that("conjugate multi value T works", {
+test_that("conjugate multi value gaussian conjugate works", {
   makeMvGauss<-function(bins=180,mu,sigma){
     setNames(data.frame(matrix(hist(rnorm(2000,mu, sigma),
                                     breaks=seq(1,bins,1), plot=FALSE)$counts, nrow=1)),
@@ -75,21 +75,71 @@ test_that("conjugate multi value T works", {
                    priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
                    plot=FALSE, rope_range = c(-5,5), rope_ci = 0.89,
                    cred.int.level = 0.89, hypothesis="equal", support=NULL)
-  
-  # summary(unlist(lapply(1:100, function(i){
-  #   #set.seed(123)
-  #   out <- conjugate(s1=mv_gauss[1:30,], s2= mv_gauss[31:70,], method="gaussian",
-  #                    priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-  #                    plot=FALSE, rope_range = c(-5,5), rope_ci = 0.89,
-  #                    cred.int.level = 0.89, hypothesis="equal")
-  #   out$summary$rope_prob
-  # })))
-  
+
   expect_equal(out$summary$post.prob, 0.7179459, tolerance = 1e-6)
   
   expect_equal(out$summary$rope_prob, 0.195, tolerance = 0.015)
   
   expect_equal(names(out), c("summary", "posterior") )
+})
+
+test_that("conjugate single value beta conjugate works", {
+  s1 <- c(0.703503634559075, 0.511635134690101, 0.632190260438834, 0.353931888757961, 
+          0.221420395118864, 0.513410161844891, 0.34422446087923, 0.377469570817219, 
+          0.553479714127415, 0.610722823397796, 0.642879912798542, 0.276682168393891, 
+          0.469471347132478, 0.444690242008423, 0.21701860450406, 0.362069754559641, 
+          0.324136767421681, 0.776072763733466, 0.678539925827321, 0.230328895808406
+  ) # dput(rbeta(20, 5, 5))
+  s2 <- c(0.609522707465377, 0.485478489613084, 0.38771181119892, 0.605383942021798, 
+          0.657651014793296, 0.650853576978649, 0.556595652577583, 0.719495963439006, 
+          0.695092908314064, 0.769699311988927, 0.665722925603491, 0.651953500947315, 
+          0.523204344509775, 0.736962689122744, 0.607863983829372, 0.703483180709229, 
+          0.418954761865872, 0.556556033829364, 0.617343726804053, 0.522669623038004
+  ) # dput(rbeta(20, 8, 5))
+  out <- conjugate(s1 = s1, s2= s2, method="beta",
+                priors = list(a=c(0.5,0.5),b=c(0.5,0.5)),
+                plot=FALSE, rope_range = c(-0.1, 0.1), rope_ci = 0.89,
+                cred.int.level = 0.89, hypothesis="equal")
+  
+  expect_equal(out$summary$post.prob, 0.02229257, tolerance = 1e-6)
+  
+  expect_equal(out$summary$rope_prob, 0.125, tolerance = 0.025)
+  
+  expect_equal(names(out), c("summary", "posterior") )
+  
+})
+test_that("conjugate multi value beta conjugate works", {
+  set.seed(123)
+  makeMvBeta<-function(n=100,a,b){
+    setNames(data.frame(matrix(hist(rbeta(2000,a,b),
+    breaks=seq(0,1,length.out=n), plot=FALSE)$counts, nrow=1)),
+    paste0("b0.",1:(n-1)))
+  }
+  
+  mv_beta<-rbind(do.call(rbind, lapply(1:30, function(i){makeMvBeta(n=100, a=5, b=8 )})),
+                 do.call(rbind, lapply(1:40, function(i){makeMvBeta(n=100, a=10, b=3 )})))
+  
+  out <- conjugate(s1 = mv_beta[1:30,], s2= mv_beta[31:70,], method="beta",
+                priors = list(a=c(0.5,0.5),b=c(0.5,0.5)),
+                plot=FALSE, rope_range = c(-0.1, 0.1), rope_ci = 0.89,
+                cred.int.level = 0.89, hypothesis="equal")
+  
+  # summary(unlist(lapply(1:100, function(i){
+  #   #set.seed(123)
+  #   out <- conjugate(s1=mv_beta[1:30,], s2= mv_beta[31:70,], method="beta",
+  #                    priors = list(a=c(0.5,0.5),b=c(0.5,0.5)),
+  #                    plot=FALSE, rope_range = c(-0.1,0.1), rope_ci = 0.89,
+  #                    cred.int.level = 0.89, hypothesis="equal")
+  #   #out$summary$post.prob
+  #   out$summary$rope_prob
+  # })))
+  
+  expect_equal(out$summary$post.prob, 1.377333e-17, tolerance = 1e-6)
+  
+  expect_equal(out$summary$rope_prob, 0, tolerance = 0.0001)
+  
+  expect_equal(names(out), c("summary", "posterior") )
+  
 })
 
 
@@ -99,9 +149,7 @@ test_that("conjugate multi value T works", {
 
 
 
-
-
-test_that("generic plotting works", {
+test_that("generic conjugate plotting works", {
   s1 = c(43.8008289810423, 44.6084228775479, 68.9524219823026, 77.442231894233, 
          45.2302703709121, 53.8005757403944, 33.8292993277826, 59.7018653972819, 
          57.8433869136181, 52.8224097917938) # dput(rnorm(10, 50,10))
@@ -119,7 +167,7 @@ test_that("generic plotting works", {
   
 })
 
-test_that("dirichlet plotting version 1 works and is consistent", {
+test_that("dirichlet conjugate plotting version 1 works and is consistent", {
   makeMvLn<-function(bins=500,mu_log,sigma_log){
   setNames(data.frame(matrix(hist(rlnorm(2000,mu_log, sigma_log),
                                   breaks=seq(1,bins,5), plot=FALSE)$counts, nrow=1)),
@@ -146,7 +194,7 @@ test_that("dirichlet plotting version 1 works and is consistent", {
 })
 
 
-test_that("dirichlet plotting version 2 works and is consistent", {
+test_that("dirichlet conjugate plotting version 2 works and is consistent", {
   makeMvLn<-function(bins=500,mu_log,sigma_log){
     setNames(data.frame(matrix(hist(rlnorm(2000,mu_log, sigma_log),
                                     breaks=seq(1,bins,5), plot=FALSE)$counts, nrow=1)),
@@ -201,64 +249,7 @@ test_that("dirichlet plotting version 2 works and is consistent", {
 #'                    plot=TRUE, rope_range = NULL, rope_ci = 0.89, 
 #'                    cred.int.level = 0.89, hypothesis="equal", support=NULL)
 #' 
-#' # Z test mv example
-#'
-#' makeMvGauss<-function(bins=180,mu,sigma){
-#'    setNames(data.frame(matrix(hist(rnorm(2000,mu, sigma),
-#'    breaks=seq(1,bins,1), plot=FALSE)$counts, nrow=1)),
-#'    paste0("b",1:(bins-1) ))
-#'    }
-#' mv_gauss<-rbind(do.call(rbind, lapply(1:30, function(i){makeMvGauss(bins=180, mu=50, sigma=10 )})),
-#'                 do.call(rbind, lapply(1:30, function(i){makeMvGauss(bins=180, mu=60, sigma=12 )})))
-#'                 
-#' gauss_mv_ex <- conjugate(s1=mv_gauss[1:30,], s2= mv_gauss[31:60,], method = "gaussian",
-#'                   priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-#'                   plot=TRUE, rope_range = c(-25, 25), rope_ci = 0.89,
-#'                   cred.int.level = 0.89, hypothesis="equal", support=NULL)
-#'                   
-#' # Z test sv example
-#' set.seed(123)
-#' gauss_sv_ex_bad <- conjugate(s1=rnorm(15, 50,10), s2= rnorm(15, 60,12), method = "gaussian",
-#'                   priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-#'                   plot=TRUE, rope_range = c(-10, 10), rope_ci = 0.89, 
-#'                   cred.int.level = 0.89, hypothesis="equal", support=NULL)
-#'                   
-#' # Here the plot clearly shows we have a problem with the default support, so we specify one
-#' # naturally the longer the support vector the more time this takes, but supports below 100k length 
-#' # tend to be reasonably fast.
-#' 
-#' gauss_sv_ex <- conjugate(s1=rnorm(15, 50,10), s2= rnorm(15, 60,12), method = "gaussian",
-#'                   priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-#'                   plot=TRUE, rope_range = c(-10, 10), rope_ci = 0.89, 
-#'                   cred.int.level = 0.89, hypothesis="equal", support=seq(-20,120, 0.01))
-#' # Note that the ROPE probability is somewhat unstable here since the distribution of differences
-#' # is much wider than the ROPE interval.
-#' 
-#' # T test mv example 
-#' 
-#' makeMvGauss<-function(bins=180,mu,sigma){
-#'    setNames(data.frame(matrix(hist(rnorm(2000,mu, sigma),
-#'     breaks=seq(1,bins,1), plot=FALSE)$counts, nrow=1)),
-#'    paste0("b",1:(bins-1) ))
-#'    }
-#' mv_gauss<-rbind(do.call(rbind, lapply(1:30, function(i){makeMvGauss(bins=180, mu=50, sigma=10 )})),
-#'                 do.call(rbind, lapply(1:30, function(i){makeMvGauss(bins=180, mu=60, sigma=12 )})))
-#'
-#' gaussianMeans_mv_ex <- conjugate(s1=mv_gauss[1:30,], s2= mv_gauss[31:60,], method="t",
-#'                         priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-#'                         plot=TRUE, rope_range = c(-5,5), rope_ci = 0.89, 
-#'                         cred.int.level = 0.89, hypothesis="equal", support=NULL)
-#'                         
-#'                         
-#' # T test sv example
-#' 
-#' gaussianMeans_sv_ex <- conjugate(s1=rnorm(10, 50,10), s2= rnorm(10, 60,12), method="t",
-#'                         priors = list( mu=c(0,0),n=c(1,1),s2=c(20,20) ),
-#'                         plot=TRUE, rope_range = c(-5,8), rope_ci = 0.89, 
-#'                         cred.int.level = 0.89, hypothesis="equal", support=NULL)
-#' 
-#' 
-#' 
+
 #' # beta mv example
 #' 
 #' makeMvBeta<-function(n=100,a,b){
@@ -275,13 +266,7 @@ test_that("dirichlet plotting version 2 works and is consistent", {
 #'               plot=TRUE, rope_range = c(-0.1, 0.1), rope_ci = 0.89, 
 #'               cred.int.level = 0.89, hypothesis="equal")
 #' 
-#' # beta sv example
-#' 
-#' beta_sv_ex <- conjugate(s1 = rbeta(20, 5, 5), s2= rbeta(20, 8, 5), method="beta",
-#'               priors = list(a=c(0.5,0.5),b=c(0.5,0.5)),
-#'               plot=TRUE, rope_range = c(-0.1, 0.1), rope_ci = 0.89, 
-#'               cred.int.level = 0.89, hypothesis="equal")
-#'
+
 #' # poisson sv example
 #' 
 #' poisson_sv_ex <- conjugate(s1 = rpois(20, 10), s2= rpois(20, 8), method="poisson",
