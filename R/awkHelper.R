f7c089fc3b77bfefa00a15793d3cc769fe531252
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-03-06 15:09:38 -0600

    styling awkHelper

--- R/awkHelper.R
+++ R/awkHelper.R
@@ -1,7 +1,7 @@
 #' subset helper function for use reading in large data, called in pcv.sub.read
-#' 
-#' 
-#' 
+#'
+#'
+#'
 #' @param inputFile Path to csv file of plantCV output, should be provided internally in read.pcv
 #' @param filters filtering conditions, see read.pcv for details. Format as
 #' list("trait in area, perimeter", "other contains stringToMatch")
@@ -15,28 +15,32 @@
 #'   which is typically passed to \code{pipe} or used as a connection in \code{data.table::fread}.
 #' @importFrom utils read.csv capture.output
 #' @export
-awkHelper<-function(inputFile, filters, awk=NULL){
-  if(is.null(awk)){
-    if(!is.list(filters)){filters<-list(filters)}
-    sed<- paste0("sed 's/\"//g' ", inputFile, " | ")
-    awkStart<-"awk -F "
-    awkDelim <-"',' "
-    awkFiltStart<-"'{ if ("
-    awkFiltEnd<-") { print } }'"
-    COLS = colnames(read.csv(inputFile, nrows=1))
-    awkFilts<-lapply(filters, function(filt){
-      filtCol = strsplit(filt," ")[[1]][1]
-      affector<-strsplit(filt," ")[[1]][2]
-      values<-trimws(gsub(",", " ", strsplit(filt," ")[[1]][-c(1:2)]))
-      if(affector %in% c("in", "is", "=")){
-        paste(paste0("($", which(COLS == filtCol), '=="', values,'")' ), collapse=" || ")
-      } else if(affector=="contains"){
-        valReg<-paste0(values, collapse="|")
-        paste0("($", which(COLS==filtCol), " ~ /", valReg, "/)")
+awkHelper <- function(inputFile, filters, awk = NULL) {
+  if (is.null(awk)) {
+    if (!is.list(filters)) {
+      filters <- list(filters)
+    }
+    sed <- paste0("sed 's/\"//g' ", inputFile, " | ")
+    awkStart <- "awk -F "
+    awkDelim <- "',' "
+    awkFiltStart <- "'{ if ("
+    awkFiltEnd <- ") { print } }'"
+    COLS <- colnames(read.csv(inputFile, nrows = 1))
+    awkFilts <- lapply(filters, function(filt) {
+      filtCol <- strsplit(filt, " ")[[1]][1]
+      affector <- strsplit(filt, " ")[[1]][2]
+      values <- trimws(gsub(",", " ", strsplit(filt, " ")[[1]][-c(1:2)]))
+      if (affector %in% c("in", "is", "=")) {
+        paste(paste0("($", which(COLS == filtCol), '=="', values, '")'), collapse = " || ")
+      } else if (affector == "contains") {
+        valReg <- paste0(values, collapse = "|")
+        paste0("($", which(COLS == filtCol), " ~ /", valReg, "/)")
       }
     })
-    awkFilt = paste(paste("(",awkFilts,")"), collapse = " && ")
-    awkCommand<-capture.output(cat(sed, awkStart, awkDelim, awkFiltStart, awkFilt, awkFiltEnd))
-  } else {awkCommand = awk}
+    awkFilt <- paste(paste("(", awkFilts, ")"), collapse = " && ")
+    awkCommand <- capture.output(cat(sed, awkStart, awkDelim, awkFiltStart, awkFilt, awkFiltEnd))
+  } else {
+    awkCommand <- awk
+  }
   return(awkCommand)
 }
