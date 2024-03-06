a2d01a2024c84f28cdc0d7e4c21bddc64b0841f0
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-03-06 15:27:04 -0600

    styling brmFamilyHelper

--- R/brmFamilyHelper.R
+++ R/brmFamilyHelper.R
@@ -1,26 +1,24 @@
 #' Functions to prepare several families of distributional brms models
-#' 
+#'
 #' @param model a model passed from growthSS
-#' 
+#'
 #' @return A list of elements to pass brmSS for fitting distributional models
-#' 
-#' @examples 
+#'
+#' @examples
 #' .brmFamilyHelper("logistic")
 #' .brmFamilyHelper("poisson: logistic")
 #' .brmFamilyHelper("von_mises: logistic")
-#' 
+#'
 #' @keywords internal
 #' @noRd
 
-.brmFamilyHelper <- function(model){
-  if(!grepl("[:]", model)){
+.brmFamilyHelper <- function(model) {
+  if (!grepl("[:]", model)) {
     model <- paste0("student:", model)
   }
   dist <- trimws(gsub(":.*", "", model))
   rhs <- trimws(gsub(".*:", "", model))
-  family = dist
+  family <- dist
   dpars <- brms::brmsfamily(dist)$dpars[-1]
   return(list(family = family, dpars = dpars, rhs = rhs))
 }
-
-
