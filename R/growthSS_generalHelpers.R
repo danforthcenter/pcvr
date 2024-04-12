3016124100b543271461206fecb789742deebd91
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-04-12 10:38:39 -0500

    intercept helper function

---
+++ R/growthSS_generalHelpers.R
@@ -0,0 +1,18 @@
+#' helper function to help make growth models including an intercept term in growthSS
+#' @examples
+#' .intModelHelper("int_logistic")
+#' .intModelHelper("logistic")
+#'
+#' @keywords internal
+#' @noRd
+
+.intModelHelper <- function(model) {
+  if(grepl("^int_", model)){
+    int <- TRUE
+    model <- gsub("^int_", "", model)
+  } else {
+    int <- FALSE
+  }
+  return(list("model" = model, "int" = int))
+}
+
