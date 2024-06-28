ab4d4c157e2f2a7d28bef02be7fffa9dbc3a5783
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-06-28 16:29:46 -0500

    cleaning names

--- R/conjugate_bivariate_uniformHelpers.R
+++ R/conjugate_bivariate_uniformHelpers.R
@@ -13,14 +13,10 @@
 #' @keywords internal
 #' @noRd
 
-s1 <- runif(10, -20, 80)
-cred.int.level = 0.95
-priors <- support <- NULL
-plot <- calculatingSupport <- FALSE
-
 .conj_bivariate_uniform_sv <- function(s1 = NULL, priors = NULL,
                              plot = FALSE, support = NULL, cred.int.level = NULL,
                              calculatingSupport = FALSE) {
+  support <- NULL
   out <- list()
   #* `make default prior if none provided`
   #* conjugate prior needs r1, r2, and alpha
@@ -29,8 +25,8 @@ plot <- calculatingSupport <- FALSE
     priors <- list(location_l = 0, location_u = 1, scale = 1)
   }
   #* `Update bivariate pareto prior with sufficient statistics`
-  (location_u_prime <- max(c(s1, priors$location_u[1])))
-  (location_l_prime <- min(c(s1, priors$location_l[1])))
+  location_u_prime <- max(c(s1, priors$location_u[1]))
+  location_l_prime <- min(c(s1, priors$location_l[1]))
   scale_prime <- priors$scale[1] + length(s1)
   #* `Define bivariate support if it is missing`
   #* `this will require some thought since there are two directions.`
@@ -105,17 +101,18 @@ plot <- calculatingSupport <- FALSE
   }
 
   #* `Store summary`
-  out$summary <- data.frame(HDE_1 = c(hde1_l, hde1_u),
+  out$summary <- data.frame(HDE_1 = c(hde_l, hde_u),
                             HDI_1_low = c(hdi_l[1], hdi_u[1]),
-                            HDI_1_high = c(hdi_l[2], hdi_u[2]))
+                            HDI_1_high = c(hdi_l[2], hdi_u[2]),
+                            param = c("A", "B"))
   out$posterior <- list("scale" = scale_prime, "location_l" = location_l_prime,
                         "location_u" = location_u_prime)
   #* `save s1 data for plotting`
   if (plot) {
     out$plot_df <- data.frame(
-      "range" = c(support_l, support_u)
+      "range" = c(support_l, support_u),
       "prob" = c(pdf_l, pdf_u),
-      "param" = rep(c("A", "B"), each = length(support_u))
+      "param" = rep(c("A", "B"), each = length(support_u)),
       "sample" = rep("Sample 1", 2 * length(support_u))
     )
   }
@@ -130,5 +127,5 @@ plot <- calculatingSupport <- FALSE
   # this is a displaced origin pareto
   # also using quantile function of the marginal x1 | x2
   x1 <- r1 + (r1 / r2) * x2 * (1 / (u ^ (1 / (scale + 1))) - 1)
-  return(list("A" = x1, "B" = x2))
+  return(cbind.data.frame("A" = x1, "B" = x2))
 }
