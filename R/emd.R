1e560ca33a9a88630310bb9322880227976a1af5
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-07-22 16:08:40 -0500

    more explicit testing and combining euc in to emd

--- R/emd.R
+++ R/emd.R
@@ -373,3 +373,12 @@ euc1d <- function(s1, s2) {
   euc <- as.numeric(stats::dist(mat, method = "euclidean"))
   return(euc)
 }
+
+#' @rdname pcv.emd
+#' @export
+#'
+pcv.euc <- function(df, cols = NULL, reorder = NULL, include = reorder, mat = FALSE, plot = TRUE,
+                    parallel = getOption("mc.cores", 1), trait = "trait", id = "image",
+                    value = "value", raiseError = TRUE, method = "euc") {
+  pcv.emd(df, cols, reorder, include, mat, plot, parallel, trait, id, value, raiseError, method)
+}
--- R/euc.R
+++
@@ -1,8 +0,0 @@
-#' @rdname pcv.emd
-#' @export
-#'
-pcv.euc <- function(df, cols = NULL, reorder = NULL, include = reorder, mat = FALSE, plot = TRUE,
-                    parallel = getOption("mc.cores", 1), trait = "trait", id = "image",
-                    value = "value", raiseError = TRUE, method = "euc") {
-  pcv.emd(df, cols, reorder, include, mat, plot, parallel, trait, id, value, raiseError, method)
-}
