12993c4b2602604cef3e1983828526f0f59f2e1a
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-08-01 14:27:26 -0500

    specifying reader

--- tests/testthat/test-long_sv_workflow.R
+++ tests/testthat/test-long_sv_workflow.R
@@ -1,7 +1,7 @@
 if (!interactive()) pdf(NULL)
 test_that("reading sv github data as long works", {
   sv <- read.pcv(paste0("https://raw.githubusercontent.com/joshqsumner/pcvrTestData/",
-                        "main/pcv4-single-value-traits.csv"), mode = "long")
+                        "main/pcv4-single-value-traits.csv"), mode = "long", reader = "read.csv")
   #* check read in
   expect_equal(dim(sv), c(77058, 20))
   expect_equal(colnames(sv), c(
