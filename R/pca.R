b85cc9f3e3e52d41eba537584ae5bb3fc849a7bd
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-08-01 09:35:59 -0500

    removing trace

--- R/pca.R
+++ R/pca.R
@@ -5,18 +5,10 @@
 #' If this is length of 1 then it is treated as regex pattern to match
 #' the column names that should be used.
 #' @param color column name(s) used to color points in the pca plot.
-#' @param trace Optional column to use to show changes over by way of geom_path,
-#'  generally this would be a time variable.
 #' @param facet Optional column or vector to facet/split plots on.
-#' If this is a character then it is taken as a column name.
-#' Numeric vectors are taken as values of trace to split on,
-#' if trace=NULL and this is numeric then plots will be split by the split argument.
-#'  If this is a vector then a list of plots is returned.
 #' @param returnData Logical, should data be returned?
 #' @param ncp Optional, number of principal components to return attached
 #'  to dataframe if data is returned. Defaults to all.
-#' @param split Time variable to split data on if a facet on time is used.
-#'  See bellwether vignette for examples.
 #' @keywords pca
 #' @details If data is returned then it will contain the coordinates from the
 #'  PCA and will not contain the columns that were reduced.
@@ -41,7 +33,7 @@
 #'
 #' @export
 
-pcadf <- function(df = NULL, cols = NULL, color = NULL, trace = NULL,
+pcadf <- function(df = NULL, cols = NULL, color = NULL,
                   facet = NULL, returnData = TRUE, ncp = NULL, split = "DAS") {
   if (is.character(cols) && length(cols) == 1) {
     cols <- which(grepl(cols, colnames(df)))
@@ -64,131 +56,23 @@ pcadf <- function(df = NULL, cols = NULL, color = NULL, trace = NULL,
   }
   pca.df <- cbind(as.data.frame(df[, -cols]), coords)
   colnames(pca.df)[1:(length(colnames(df)) - length(cols))] <- colnames(df)[-cols]
-  if (!is.null(trace)) {
-    pca.df <- pca.df[sort(pca.df[[trace]], index.return = TRUE)$ix, ]
+  facetLayer <- NULL
+  if (!is.null(facet)) {
+    facetLayer <- ggplot2::facet_wrap(as.formula(paste0("~", paste(facet, collapse = "+"))))
   }
-  facetHelperList <- .pcaFacetHelper(facet, trace, split)
-  facetLayer <- facetHelperList[["facetLayer"]]
-  additive <- facetHelperList[["additive"]]
-  trace <- facetHelperList[["trace"]]
-  traceSplit <- facetHelperList[["traceSplit"]]
-  traceSplits <- facetHelperList[["traceSplits"]]
-  traceDraw <- facetHelperList[["traceDraw"]]
 
   if (is.null(color)) {
     pca.df$dummyVariableForColor <- 1
     color <- "dummyVariableForColor"
   }
 
-  if (traceSplit) {
-    plots <- .pcaTracePlot(
-      pca.df, trace, color, facetLayer,
-      traceSplits, pc1Var, pc2Var, traceDraw, additive
-    )
-  } else {
-    plots <- .pcaGeneralPlot(pca.df, color, facetLayer, pc1Var, pc2Var)
-  }
+  plots <- .pcaGeneralPlot(pca.df, color, facetLayer, pc1Var, pc2Var)
 
   if (returnData) {
     return(list("data" = pca.df, "pca" = pca, "plot" = plots))
   } else {
     return(plots)
   }
-}
-
-
-
-
-.pcaFacetHelper <- function(facet, trace, split) {
-  traceSplit <- FALSE
-  additive <- TRUE
-  facetLayer <- NULL
-  traceSplits <- NULL
-  traceDraw <- FALSE
-  facetLayer <- NULL
-  if (!is.null(facet)) {
-    if (is.character(facet)) {
-      facetLayer <- ggplot2::facet_wrap(as.formula(paste0("~", paste(facet, collapse = "+"))))
-    } else if (is.numeric(facet)) {
-      traceSplit <- TRUE
-      traceSplits <- facet
-      traceDraw <- TRUE
-      if (is.null(trace)) {
-        trace <- split
-        traceDraw <- FALSE
-        additive <- FALSE
-      }
-    }
-  }
-  return(list(
-    "facetLayer" = facetLayer,
-    "additive" = additive,
-    "trace" = trace,
-    "traceSplit" = traceSplit,
-    "traceSplits" = traceSplits,
-    "traceDraw" = traceDraw
-  ))
-}
-
-#' traced pca plotting
-#' @keywords internal
-#' @noRd
-
-.pcaTracePlot <- function(pca.df, trace, color, facetLayer,
-                          traceSplits, pc1Var, pc2Var, traceDraw, additive) {
-  trace_limit_x <- c(
-    min(pca.df$pc1) - (min(pca.df$pc1) * .05),
-    max(pca.df$pc1) + (max(pca.df$pc1) * .05)
-  )
-  trace_limit_y <- c(
-    min(pca.df$pc2) - (min(pca.df$pc2) * .05),
-    max(pca.df$pc2) + (max(pca.df$pc2) * .05)
-  )
-
-  plots <- lapply(seq_along(traceSplits), function(i) {
-    maxTrace <- traceSplits[i]
-    if (i == 1) {
-      prevTrace <- min(pca.df[[trace]])
-    } else {
-      prevTrace <- traceSplits[i - 1]
-    }
-    if (additive) {
-      if (i == length(traceSplits)) {
-        through <- "+"
-      } else {
-        through <- paste0(" through ", traceSplits[i + 1])
-      }
-      TITLE <- paste0(trace, ": ", maxTrace, through)
-      pca.df.sub <- pca.df[pca.df[[trace]] <= maxTrace, ]
-    } else {
-      TITLE <- paste(trace, maxTrace)
-      pca.df.sub <- pca.df[pca.df[[trace]] == maxTrace, ]
-    }
-
-    p <- ggplot2::ggplot(
-      pca.df.sub,
-      ggplot2::aes(x = .data$pc1, y = .data$pc2, color = .data[[color]])
-    ) +
-      ggplot2::geom_point() +
-      ggplot2::labs(
-        x = paste0("PC 1 (", pc1Var, "%)"), y = paste0("PC 2 (", pc2Var, "%)"),
-        title = TITLE
-      ) +
-      pcv_theme() +
-      coord_cartesian(xlim = trace_limit_x, ylim = trace_limit_y)
-    if (color == "dummyVariableForColor") {
-      p <- p + ggplot2::theme(legend.position = "none")
-    }
-    if (!is.null(trace) & traceDraw) {
-      p <- p + ggplot2::geom_path(
-        data = pca.df[pca.df[[trace]] >= prevTrace & pca.df[[trace]] <= maxTrace, ],
-        ggplot2::aes(group = .data[[color]]), linewidth = 0.15, show.legend = FALSE
-      )
-    }
-    p <- p + facetLayer
-    return(p)
-  })
-  return(plots)
 }
 
 #' general pca plotting
@@ -205,11 +89,6 @@ pcadf <- function(df = NULL, cols = NULL, color = NULL, trace = NULL,
     pcv_theme()
   if (color == "dummyVariableForColor") {
     plots <- plots + ggplot2::theme(legend.position = "none")
-  }
-  if (!is.null(trace)) {
-    plots <- plots + ggplot2::geom_path(ggplot2::aes(group = .data[[color]]),
-      linewidth = 0.15, show.legend = FALSE
-    )
   }
   plots <- plots + facetLayer
   return(plots)
