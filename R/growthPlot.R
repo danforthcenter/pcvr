7a0c3d7584c1fd05dcb8c5a16e23af8a9bfbd8bc
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-10-24 08:53:15 -0500

    checking for gam models while plotting

--- R/growthPlot.R
+++ R/growthPlot.R
@@ -62,7 +62,8 @@ growthPlot <- function(fit, form, groups = NULL, df = NULL, timeRange = NULL,
     model_class <- class(fit[[1]])
   }
   if (methods::is(fit, "brmsfit")) {
-    if (attr(fit$formula$formula, "nl")) { # non linear models are growth models
+    if (attr(fit$formula$formula, "nl") || as.logical(length(fit$basis$dpars[[fit$family$dpars[1]]]))) {
+      # non linear models are growth models, gams are linear but have spline dpars for the first param
       plot <- brmPlot(
         fit = fit, form = form, groups = groups, df = df, timeRange = timeRange,
         facetGroups = facetGroups, hierarchy_value = hierarchy_value, vir_option = virMaps[1]
