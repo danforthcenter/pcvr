dbdf236c036fe51ef1e9d796191abe04ec329739
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-11-05 15:06:52 -0600

    allowing mvSim to make longitudinal data

--- R/mvSim.R
+++ R/mvSim.R
@@ -16,6 +16,13 @@
 #' @param wide Boolean, should data be returned in wide format (the default)?
 #' If FALSE then long data is returned.
 #' @param binwidth How wide should bins be? Defaults to 1.
+#' @param t Number of timepoints to simulate. Defaults to NULL in which case data is simulated as
+#' non-longitudinal. Note that currently the first non \code{n} argument of the data simulating
+#' function is assumed to be the parameter changing over time (ie, mean in rnorm, meanlog in rlnorm).
+#' @param model A type of growth model, passed to \link{growthSim}. This is only used if t is
+#' specified.
+#' @param params Parameters for the growth model, passed to \link{growthSim}. This is also only used
+#' if t is specified. Note growth will start from the values specified in dists. See examples.
 #' @keywords multi-value
 #' @return Returns a dataframe of example multi-value trait data simulated from specified distributions.
 #'
@@ -26,10 +33,6 @@
 #'
 #' library(extraDistr) # for rmixnorm
 #' library(ggplot2)
-#' n_samples = 10
-#' counts = 1000
-#' min_bin = 0
-#' max_bin = 180
 #' dists <- list(
 #'   rmixnorm = list(mean = c(70, 150), sd = c(15, 5), alpha = c(0.3, 0.7)),
 #'   rnorm = list(mean = 90, sd = 3)
@@ -46,20 +49,44 @@
 #'   geom_col(position = "identity", alpha = 0.25) +
 #'   pcv_theme() +
 #'   labs(x = "bin")
+#' dists = list(rnorm = list(mean = 30, sd = 15), rnorm = list(mean = 25, sd = 10))
+#' x3 <- mvSim(dists = dists, wide = FALSE, # here we make longitudinal data
+#'             t = 10, model = "linear", params = list("A" = c(10, 5)))
+#' ggplot(x3, aes(
+#'   x = as.numeric(sub("sim_", "", variable)),
+#'   y = value, group = interaction(group, id), fill = group)) +
+#' facet_wrap(~times) +
+#' geom_col(position = "identity", alpha = 0.25) +
+#' pcv_theme() +
+#' labs(x = "bin")
+#'
 #'
 #' @export
 
 mvSim <- function(dists = list(rnorm = list(mean = 100, sd = 15)),
                   n_samples = 10, counts = 1000, min_bin = 1, max_bin = 180, wide = TRUE,
-                  binwidth = 1) {
+                  binwidth = 1, t = NULL, model = "linear", params = list("A" = 10)) {
   if (length(n_samples) == 1) {
     n_samples <- rep(n_samples, length(dists))
   }
-  vecs <- .makeVecs(dists, counts, n_samples)
-  out <- .simFreqs(vecs, max_bin, min_bin, binwidth)
+  if (!is.null(t)) {
+    n_samples <- rep(n_samples, t)
+  }
+  names(dists) <- paste(names(dists), seq_along(dists), sep = "_")
+  res <- .longitudinalDists(dists, t, model, params)
+  dists2 <- res$dist
+  times <- res$times
+  vecs <- .makeVecs(dists2, counts, n_samples)
+  out <- .simFreqs(vecs, max_bin, min_bin, binwidth, times)
   if (!wide) {
-    out$id <- seq_len(nrow(out))
-    out <- as.data.frame(data.table::melt(data.table::as.data.table(out), id.vars = c("group", "id")))
+    out <- cbind(data.frame(id = seq_len(nrow(out))),
+                 out)
+    out <- as.data.frame(
+      data.table::melt(
+        data.table::as.data.table(out),
+        id.vars = seq_len(min(which(grepl("sim_", colnames(out))))-1)
+        )
+      )
   }
   return(out)
 }
@@ -69,12 +96,12 @@ mvSim <- function(dists = list(rnorm = list(mean = 100, sd = 15)),
 #' @noRd
 
 .makeVecs <- function(dists, counts, n_samples) {
-  funs <- names(dists)
+  funs <- sub("_[0-9]+$", "", names(dists))
   out <- lapply(seq_along(funs), function(i) {
     f <- funs[i]
     fun <- match.fun(f)
     dists[[i]]$n <- counts
-    lapply(1:n_samples[i], function(e) {
+    lapply(seq_len(n_samples[i]), function(e) {
       do.call(fun, args = dists[[i]])
     })
   })
@@ -86,12 +113,12 @@ mvSim <- function(dists = list(rnorm = list(mean = 100, sd = 15)),
 #' @keywords internal
 #' @noRd
 
-.simFreqs <- function(vecs, max_bin, min_bin, binwidth) {
-  do.call(rbind, lapply(seq_along(vecs), function(i) {
+.simFreqs <- function(vecs, max_bin, min_bin, binwidth, times) {
+  out <- do.call(rbind, lapply(seq_along(vecs), function(i) {
     vecName <- names(vecs)[i]
     vec <- vecs[[i]]
     do.call(rbind, lapply(vec, function(v) {
-      if (vecName == "rbeta") {
+      if (grepl("rbeta", vecName)) {
         v[v > 1] <- 1
         v[v < 0] <- 0
         s1 <- hist(v, breaks = seq(0, 1, length.out = 100), plot = FALSE)$counts
@@ -107,4 +134,41 @@ mvSim <- function(dists = list(rnorm = list(mean = 100, sd = 15)),
       s1d
     }))
   }))
+  if (any(as.logical(times))) {
+    out <- cbind(
+      data.frame(times = rep(times, each = unique(unlist(lapply(vecs, length)))[1])),
+      out
+    )
+  }
+  return(out)
 }
+
+#' interal function to replicate dists for longitudinal data
+#' @keywords internal
+#' @noRd
+
+.longitudinalDists <- function(dists, t, model, params) {
+  times <- 0
+  if (!is.null(t)) {
+    pred <- growthSim(model = model, n = 1, t = t, params = params)[, c("group", "y")]
+    pred$group <- sapply(pred$group, function(i) {which(letters == i)})
+    times <- rep(seq_len(t), times = length(dists))
+    nms <- sub("_[0-9]+$", "", names(dists))
+    dists <- Reduce(append, lapply(seq_along(nms), function(i) {
+      nm <- nms[i]
+      args <- names(formals(nm))
+      args <- args[-which(args == "n")]
+      vary <- args[1]
+      temporal_means <- dists[[i]][[vary]] + pred[pred$group == i, "y"]
+      tdists <- lapply(temporal_means, function(tm) {
+        diter <- dists[[i]]
+        diter[[vary]] <- tm
+        diter
+      })
+      names(tdists) <- rep(names(dists)[i], t)
+      tdists
+    }))
+  }
+  return(list("dist" = dists, "times" = times))
+}
+
