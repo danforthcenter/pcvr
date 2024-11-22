edfc5229668cc53e65eb7c3488fbddcf1a250fa2
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2024-11-22 11:36:49 -0600

    docs update

--- R/brmPlot.R
+++ R/brmPlot.R
@@ -15,7 +15,9 @@
 #' although prediction using splines outside of the observed range is not necessarily reliable.
 #' @param facetGroups logical, should groups be separated in facets? Defaults to TRUE.
 #' @param hierarchy_value If a hierarchical model is being plotted, what value should the
-#' hiearchical predictor be? If left NULL (the default) the mean value is used.
+#' hiearchical predictor be? If left NULL (the default) the mean value is used. If this is >1L
+#' then the x axis will use the hierarchical variable from the model at the mean of the timeRange
+#' (mean of x values in the model if timeRange is not specified).
 #' @param vir_option Viridis color scale to use for plotting credible intervals. Defaults to "plasma".
 #' @keywords growth-curve brms
 #' @import ggplot2
