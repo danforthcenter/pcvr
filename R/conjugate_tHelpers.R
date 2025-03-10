e163161edf5a60013a921f56ac5bc7df4d1c6c70
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-03-10 07:59:00 -0500

    changing MV to match new SV

--- R/conjugate_tHelpers.R
+++ R/conjugate_tHelpers.R
@@ -46,10 +46,10 @@
   hde1_mean <- mu_prime
   hdi1_mean <- qnorm(c((1 - cred.int.level) / 2, (1 - ((1 - cred.int.level) / 2))),
                      mu_prime, sd_prime)
-
+  #* `Make Summary and Posterior for output`
   out$summary <- data.frame(HDE_1 = hde1_mean, HDI_1_low = hdi1_mean[1], HDI_1_high = hdi1_mean[2])
   out$posterior$mu <- m1_n
-  out$posterior$sd <- sd_prime # return variance
+  out$posterior$sd <- sd_prime
   out$prior <- priors
   #* `Make Posterior Draws`
   out$posteriorDraws <- rnorm(10000, mu_prime, sd_prime)
@@ -99,57 +99,53 @@
   out <- list()
   #* `make default prior if none provided`
   if (is.null(priors)) {
-    priors <- list(mu = 0, n = 1, s2 = 100)
+    priors <- list(mu = 0, sd = 10)
   }
   #* `Reorder columns if they are not in the numeric order`
   histColsBin <- as.numeric(sub("[a-zA-Z_.]+", "", colnames(s1)))
   bins_order <- sort(histColsBin, index.return = TRUE)$ix
   s1 <- s1[, bins_order]
-
   #* `Turn s1 matrix into a vector`
   X1 <- rep(histColsBin[bins_order], as.numeric(round(colSums(s1))))
-
-  #* `Get Mean, Variance, SE, and DF from s2`
-  n1 <- nrow(s1) # n samples
-  m1 <- mean(X1) # xbar
-  s2_1 <- var(X1) # var
-
-  v1 <- priors$n[1] - 1 # prior DF
-  n1_n <- priors$n[1] + n1 # total N including prior
-  m1_n <- (n1 * m1 + priors$n[1] * priors$mu[1]) / n1_n # weighted mean of prior and data
-  v1_n <- v1 + n1 # degrees of freedom including data
-  s2_1_n <- ((n1 - 1) * s2_1 + v1 * priors$s2[1] + priors$n[1] * n1 * (priors$mu[1] - m1)^2 / n1_n) /
-    v1_n # pooled variance
-  se1 <- sqrt(s2_1_n / n1_n) # standard error of the mean
+  #* `Calculate Sufficient Statistics`
+  #* See notes in SV version above
+  n <- nrow(s1)
+  xbar <- mean(X1)
+  var_data <- var(X1)
+  #* `Get Prior Information`
+  var_null <- priors$sd[1] ^ 2
+  mu_null <- priors$mu[1]
+  #* `Update N(mu', var') of Mu in N(Mu, Var)`
+  mu_prime <- (var_null / ((var_data / n) + var_null)) * xbar +
+    (var_data / ((var_data / n) + var_null)) * mu_null
+  var_prime <- ((1 / var_null) + (n / var_data)) ^ -1
+  sd_prime <- sqrt(var_prime)
   #* `Define support if it is missing`
   if (is.null(support) && calculatingSupport) {
-    quantiles <- qlst(c(0.0001, 0.9999), v1_n, m1_n, se1)
+    quantiles <- qnorm(c(0.0001, 0.9999), mu_prime, sd_prime)
     return(quantiles)
   }
-  dens <- extraDistr::dlst(support, v1_n, m1_n, se1)
+  dens <- qnorm(support, mu_prime, sd_prime)
   pdf1 <- dens / sum(dens)
-  hde1_mean <- m1_n
-  hdi1_mean <- m1_n + qt(c((1 - cred.int.level) / 2, (1 - ((1 - cred.int.level) / 2))), v1_n) * se1
-
+  hde1_mean <- mu_prime
+  hdi1_mean <- qnorm(c((1 - cred.int.level) / 2, (1 - ((1 - cred.int.level) / 2))),
+                     mu_prime, sd_prime)
+  #* `Make Summary and Posterior for output`
   out$summary <- data.frame(HDE_1 = hde1_mean, HDI_1_low = hdi1_mean[1], HDI_1_high = hdi1_mean[2])
   out$posterior$mu <- m1_n
-  out$posterior$n <- n1_n
-  out$posterior$s2 <- s2_1_n
+  out$posterior$sd <- sd_prime
   out$prior <- priors
   #* `Make Posterior Draws`
-  out$posteriorDraws <- extraDistr::rlst(10000, v1_n, m1_n, se1)
+  out$posteriorDraws <- rnorm(10000, mu_prime, sd_prime)
   out$pdf <- pdf1
   #* `Save data for plotting`
   out$plot_list <- list(
     "range" = support,
-    "ddist_fun" = "extraDistr::dlst",
-    "priors" = list("df" = max(c(2, priors$n[1])),
-                    "mu" = priors$mu[1],
-                    "sigma" = sqrt(priors$s2[1] / priors$n[1])),
-    "parameters" = list("df" = v1_n,
-                        "mu" = m1_n,
-                        "sigma" = se1)
+    "ddist_fun" = "stats::rnorm",
+    "priors" = list("mu" = priors$mu[1],
+                    "sd" = priors$sd[1]),
+    "parameters" = list("mu" = mu_prime,
+                        "sd" = sd_prime)
   )
-
   return(out)
 }
