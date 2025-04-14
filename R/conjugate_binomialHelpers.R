36fe4be6e43bb958756382fe01e84283f85e5a7e
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-04-14 08:32:37 -0500

    returning sizes for nbinom

--- R/conjugate_binomialHelpers.R
+++ R/conjugate_binomialHelpers.R
@@ -63,7 +63,8 @@
     "ddist_fun" = "stats::dbeta",
     "priors" = list("shape1" = priors$a[1],  "shape2" = priors$b[1]),
     "parameters" = list("shape1" = a1_prime,
-                        "shape2" = b1_prime)
+                        "shape2" = b1_prime),
+    "given" = list("size" = round(mean(s1_trials)))
   )
   return(out)
 }
