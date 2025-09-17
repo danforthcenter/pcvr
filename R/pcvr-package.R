60733deca2699af7a4a4d4fc53f6b4dc844fc1e7
Josh Sumner <51797700+joshqsumner@users.noreply.github.com>
2025-09-17 09:31:46 -0500

    package level doc

---
+++ R/pcvr-package.R
@@ -0,0 +1,25 @@
+#' Plant Phenotyping and Bayesian Statistics
+#'
+#' @name pcvr-package
+#' @aliases pcvr
+#'
+#' @description
+#' \if{html}{
+#'    \figure{pcvr_logo.png}{options: width="100" alt="logo" style="float: right"}
+#' }
+#'
+#' The \pkg{pcvr} package is meant to interface with any datatypes returned from \pkg{PlantCV}
+#' including single and multi value trait phenotypes as well as common metadata for plant science
+#' experiments as well as lower the barrier to entry for select Bayesian statistics and longitudinal
+#' models of particular use in plant science.
+#'
+#'
+#' @seealso
+#' Useful links:
+#' \itemize{
+#' \item \url{https://plantcv.org}
+#' \item Report bugs at \url{https://github.com/danforthcenter/pcvr}
+#' }
+#' \code{\link{growthSS}}
+#' \code{\link{conjugate}}
+"_PACKAGE"
